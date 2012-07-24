bc95fa685a0c2e39086592c25111e2989bf553cc
Julian Schuh <julez@julez.in>
2012-07-25 01:19:43 +0200

    Fixes

--- lib/socky/server/application.rb
+++ lib/socky/server/application.rb
@@ -55,7 +55,7 @@ module Socky
           digest = OpenSSL::Digest::SHA256.new
           hashed_data = OpenSSL::HMAC.hexdigest(digest, @secret, json_data)
 
-          EventMachine::HttpRequest.new(@webhook_url).post body: json_data, head: { 'data-hash' => hashed_data } rescue InvalidURIError
+          EventMachine::HttpRequest.new(@webhook_url).post body: json_data, head: { 'data-hash' => hashed_data } rescue nil
       end
     end
   end
