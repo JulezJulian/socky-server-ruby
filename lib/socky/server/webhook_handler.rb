68bb623266df58f1d28c0ed57e1709192bd73b58
Julian Schuh <julez@julez.in>
2012-07-25 13:04:59 +0200

    Fixes

--- lib/socky/server/webhook_handler.rb
+++ lib/socky/server/webhook_handler.rb
@@ -24,13 +24,14 @@ module Socky
       protected
         def send_data
           return if @application.webhook_url.nil?
-          return if @events.blank?
+          return if @events.empty?
 
           json_data = events.to_json
-
           hash = sign_data(json_data)
 
           EventMachine::HttpRequest.new(@webhook_url).post body: json_data, head: { 'data-hash' => hash } rescue nil
+
+          @events.clear
         end
 
         def sign_data(data)
